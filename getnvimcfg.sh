8a9bd1b85f449f9cc80d8496dca2f57addab8543
MosesDVarghese <accydent@gmail.com>
2024-10-05 17:01:50 +0400

    up nvim scr

--- getnvimcfg.sh
+++ getnvimcfg.sh
@@ -8,15 +8,11 @@ mkdir -p ~/backups9m/nvim && cp -r ~/.config/nvim/* ~/backups9m/nvim/
 echo "Removing current Neovim configuration..."
 rm -rf ~/.config/nvim/*
 
-# Step 3: Clone the dotfiles repository
-echo "Cloning dotfiles repository..."
-git clone https://github.com/MosesDVarghese/dotfiles-1 ~/dotfiles-1
-
-# Step 4: Remove the .git folder from the cloned repository
+# Step 3: Remove the .git folder from the cloned repository
 echo "Removing .git folder from cloned repository..."
 rm -rf ~/dotfiles-1/.git
 
-# Step 5: Copy the Neovim config from the cloned repo to the Neovim config location
+# Step 4: Copy the Neovim config from the cloned repo to the Neovim config location
 echo "Copying new Neovim configuration..."
 cp -r ~/dotfiles-1/.config/nvim ~/.config
 
